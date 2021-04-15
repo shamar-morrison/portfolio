e1874b7e7ee356212ccb29f956469408484481f1
shamar-morrison <shamar.webdev@gmail.com>
2021-04-15 13:30:29 -0500

    Fixed media queries

--- src/js/main.js
+++ src/js/main.js
@@ -16,13 +16,23 @@ const navbarObserver = new IntersectionObserver(
 
 navbarObserver.observe(heroSec);
 
-/** Auto-Update Copyright Date */
-const copyrightDate = new Date().getFullYear();
-document.querySelector('.copyright-date').textContent = copyrightDate;
-
-/** Hide Preloader GIF */
+/** onload */
 window.onload = function () {
+	// Remove Preloader Gif
 	document.querySelector('.preloader').style.opacity = '0';
 	document.querySelector('.preloader').classList.add('invisible');
 	document.querySelector('body').style.overflowY = 'scroll';
+
+	/** Apply sticky navbar if on mobile */
+	const viewportwidth = document.documentElement.clientWidth;
+	console.debug('vp width', viewportwidth);
+
+	if (viewportwidth <= 992) {
+		navbar.classList.add('sticky');
+		navbarObserver.unobserve(heroSec);
+	}
 };
+
+/** Auto-Update Copyright Date */
+const copyrightDate = new Date().getFullYear();
+document.querySelector('.copyright-date').textContent = copyrightDate;
