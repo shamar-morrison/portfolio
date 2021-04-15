d0b3aa1f7550cb6f77bd0b180c80f5b6448f9a9e
shamar-morrison <shamar.webdev@gmail.com>
2021-04-15 16:14:41 -0500

    Add mobile menu

--- src/js/main.js
+++ src/js/main.js
@@ -1,5 +1,7 @@
 const heroSec = document.querySelector('#hero');
 const navbar = document.querySelector('.navbar');
+const mobileMenu = document.querySelector('.mobile__menu--container');
+const mobileMenuLinks = document.querySelector('.mobile__menu--links');
 
 /** Apply sticky navbar style on scroll */
 const navbarObserver = new IntersectionObserver(
@@ -13,7 +15,6 @@ const navbarObserver = new IntersectionObserver(
 	},
 	{ threshold: 0.85 }
 );
-
 navbarObserver.observe(heroSec);
 
 /** onload */
@@ -22,17 +23,22 @@ window.onload = function () {
 	document.querySelector('.preloader').style.opacity = '0';
 	document.querySelector('.preloader').classList.add('invisible');
 	document.querySelector('body').style.overflowY = 'scroll';
-
-	/** Apply sticky navbar if on mobile */
-	const viewportwidth = document.documentElement.clientWidth;
-	console.debug('vp width', viewportwidth);
-
-	if (viewportwidth <= 992) {
-		navbar.classList.add('sticky');
-		navbarObserver.unobserve(heroSec);
-	}
 };
 
 /** Auto-Update Copyright Date */
 const copyrightDate = new Date().getFullYear();
 document.querySelector('.copyright-date').textContent = copyrightDate;
+
+/** Mobile Menu */
+const mobileMenuCallback = function () {
+	if (mobileMenu.classList.contains('open')) {
+		mobileMenu.style.transform = 'translateX(-100%)';
+		mobileMenu.classList.remove('open');
+	} else {
+		mobileMenu.style.transform = 'translateX(0)';
+		mobileMenu.classList.add('open');
+	}
+};
+document.querySelector('.navbar-toggler').addEventListener('click', mobileMenuCallback);
+document.querySelector('.mobile__menu--close').addEventListener('click', mobileMenuCallback);
+mobileMenuLinks.addEventListener('click', mobileMenuCallback);
